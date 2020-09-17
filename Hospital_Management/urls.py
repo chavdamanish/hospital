c661936322acd2791bd339513e39dde763e8080b
chavdamanish <chavdamanish1992@gmail.com>
2020-09-17 10:51:04 +0530

    First Commit

---
+++ Hospital_Management/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path
+from hospital.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('about/', About, name='about'),
+    path('contact/', Contact, name='contact'),
+    path('', Index, name='home'),
+    path('admin_login/', Login, name='login'),
+    path('logout/', Logout_admin, name='logout'),
+    path('view_doctor/', View_Doctor, name='view_doctor'),
+    path('add_doctor/', Add_Doctor, name='add_doctor'),
+    path('delete_doctor(?P<int:pid>)/', Delete_Doctor, name='delete_doctor'),
+    path('add_patient/', Add_Patient, name='add_patient'),
+    path('view_patient/', View_Patient, name='view_patient'),
+    path('delete_patient(?P<int:pid>)/', Delete_Patient, name='delete_patient'),
+    path('add_appointment/', Add_Appointment, name='add_appointment'),
+    path('view_appointment/', View_Appointment, name='view_appointment'),
+    path('delete_appointment(?P<int:pid>)/', Delete_Appointment, name='delete_appointment'),
+]
